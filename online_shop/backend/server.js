6d4fe3bbcbe803ae907924dc23ea43d1630028a9
PolliShell <polliandfibi@gmail.com>
2024-05-07 10:40:38 +0300

    add basic auth and google auth

--- online_shop/backend/server.js
+++ online_shop/backend/server.js
@@ -8,6 +8,7 @@ const authRouter = require("./routes/authRoutes");
 require("dotenv").config();
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true }));
